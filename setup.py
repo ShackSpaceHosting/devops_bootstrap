f8142fe2c0b8b12d06548c23ded6292bc1c83e9d
David Wimsey <david@wimsey.us>
2018-02-06 23:22:47 -0500

    Splitting out roles to their own repositories and fixing up various playbooks and inventories

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
+
+# sudo yum install epel-release
+# sudo yum -y update
+# sudo yum -y install python-pip
+# sudo pip install --upgrade pip
+# sudo yum install ansible pycrypto
+#
+
 # import os
 import os.path
 import sys
@@ -7,7 +15,7 @@ import subprocess
 
 sys.path.insert(0, os.path.abspath('lib'))
 
-roles_path = 'roles/'
+roles_path = os.path.dirname(os.path.realpath(__file__)) + '/ansible/roles/'
 
 with open('requirements.txt') as requirements_file:
 	install_requirements = requirements_file.read().splitlines()
@@ -17,14 +25,14 @@ with open('requirements.txt') as requirements_file:
 
 	subprocess.call(['pip', 'install', '--user', '--upgrade', '-r','requirements.txt'])
 
-
 try:
 	from ansible.release import __version__, __author__
 except ImportError:
-	print("Ansible is required for this repository to be useful, attempting to install: ")
+	print("Ansible is not properly installed.")
 	sys.exit(1)
 
-with open('requirements.yml') as stream:
+
+with open(roles_path + '/requirements.yml') as stream:
 	requirements_list = []
 	try:
 		requirements_list = yaml.load(stream)
@@ -36,4 +44,4 @@ with open('requirements.yml') as stream:
 		if 'src' in req and 'name' in req and 'scm' in req and ['src'] is not None and req['name'] is not None and req['scm'] is not None and req['scm'] == 'git':
 			subprocess.call(['git', 'clone', req['src'], roles_path + req['name']])
 
-	subprocess.call(['ansible-galaxy', 'install', '-r', 'requirements.yml', '--ignore-errors'])
+	subprocess.call(['ansible-galaxy', 'install', '-r', 'requirements.yml', '--ignore-errors'], cwd=roles_path)
